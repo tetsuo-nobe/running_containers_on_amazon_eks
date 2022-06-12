0f0a223b338fd15f4c057f97f5ec7378c0334581
Tetsuo Nobe <tnobe@amazon.co.jp>
2022-06-12 21:03:10 +0900

    fix mod5

---
+++ mod5/X-Ray/src/app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template
+import json
+import boto3
+import datetime
+import os
+import logging
+#
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+#
+from aws_xray_sdk.core import patch
+#
+from botocore.config import Config
+from botocore.exceptions import NoCredentialsError,ClientError
+patch(['boto3'])
+
+app = Flask(__name__)
+app.logger.setLevel(logging.INFO)
+
+#
+xray_recorder.configure(service='python-web-xray')
+XRayMiddleware(app, xray_recorder)
+#
+
+@app.route('/')
+def index():
+    #
+    return render_template('index.html')
+
+@app.route('/<name>')
+def hello(name):
+    # キューの名前を指定してインスタンスを取得
+    try:
+      sqs = boto3.resource('sqs', region_name="ap-northeast-1")
+      qname = 'Demo-XRay-Q'
+      queue = sqs.get_queue_by_name(QueueName=qname)
+      queue.send_message(MessageBody=name)
+    except NoCredentialsError as nocrederr:
+      app.logger.error("!!!! InvalidCredentials !!!!")
+      app.logger.error(nocrederr)
+    except ClientError as clienterr:
+      app.logger.error('!!!! ClientError !!!!')
+      app.logger.error(clienterr)
+      error_code = clienterr.response['Error']['Code']
+      app.logger.error('error_code=%s',error_code)
+    except Exception as ex:
+      app.logger.error('!!!! Exception !!!!')
+      app.logger.error(ex)
+    #
+    return render_template('hello.html', title='Hello Page', name=name)
+
+if __name__ == "__main__":
+  app.run(debug=True, host='0.0.0.0', port=8000)
